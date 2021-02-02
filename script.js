b6e62c3df6c773be86cb4821bc447a3d246b33f0
ThomasTW935 <thomastw935@gmail.com>
2021-02-02 21:54:29 +0800

    Mobile V Completedd

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
+let priceRange = [
+  { views: '10K', cost: 8 },
+  { views: '50K', cost: 12 },
+  { views: '100K', cost: 16 },
+  { views: '500k', cost: 24 },
+  { views: '1M', cost: 36 }
+]
+
+let views = document.querySelector('.priceComponent__views')
+let cost = document.querySelector('.priceComponent__cost')
+let inputRange = document.querySelector('.priceComponent__inputRange')
+let initialValue = sessionStorage.index ?? 2
+let discountToggle = document.querySelector('.priceComponent__toggle')
+
+
+init()
+
+function init() {
+  setInputRangeValue(initialValue)
+  inputRangeEvent()
+}
 
 function inputRangeEvent() {
-  let inputRange = document.querySelector('.priceComponent__inputRange')
-
   inputRange.addEventListener('change', () => {
-    console.log(inputRange.value)
     setInputRangeValue(inputRange.value)
   })
 }
-
-function setInputRangeValue(index = 2) {
-  let priceRange = [
-    { views: '10K', cost: 8 },
-    { views: '50K', cost: 12 },
-    { views: '100K', cost: 16 },
-    { views: '500k', cost: 24 },
-    { views: '1M', cost: 36 }
-  ]
-  let views = document.querySelector('.priceComponent__views')
-  let cost = document.querySelector('.priceComponent__cost')
+function setInputRangeValue(index) {
   cost.innerHTML = '$' + priceRange[index].cost
   views.innerHTML = priceRange[index].views
+  inputRange.value = index
+  setSessionStorage(index)
+}
+function setSessionStorage(value) {
+  sessionStorage.setItem('index', value)
 }
 
-setInputRangeValue()
-inputRangeEvent()
+
